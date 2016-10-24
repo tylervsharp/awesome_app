e768a91ee456507719d8c19488384e9833271688
[01;31m[K"name"[m[K: "Tyler Sharp <[01;31m[K"email"[m[K: "tyler.v.sharp@gmail.com>
2016-10-24 17:32:35 -0400

    Implement Materialize on form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'jobs#index'
   resources :jobs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161024144433) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
